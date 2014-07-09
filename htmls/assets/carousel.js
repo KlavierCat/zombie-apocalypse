209fb19d08f355bb833e91186c26994f9d6fe2cb
Kyle Murphy <kyle-murphy89@live.com>
2014-07-09 16:55:10 +0100

    Very rough prototype of inventory
    Still needs to be styled, background colour is more so I can see what's
    happening. Borders so I can see how everything is laid out, they will be
    commented out later

---
+++ htmls/assets/carousel.js
@@ -0,0 +1,35 @@
+$(function(){
+var speed = 600,
+    currSel = 0,
+    itemCount = $('.carousel ul li')
+                    .length,
+    itemWidth = $('.carousel ul li')
+                  .css('width')
+                    .split('px')[0] ;
+
+$('.navNext').on('click',function(){
+  currSel = (currSel+1)%itemCount;
+
+  //while less than itemCount currSel will always increment by 1
+  //when equal to itemCount it will return to the 0th index
+  //modulus is a useful operator for iterating through an array that needs to reset itself
+
+
+  //console.log((currSel*itemWidth));
+  $('.carousel ul').animate({marginLeft: '-' +(currSel*itemWidth) +'px'},speed);
+});
+
+$('.navPrev').on('click',function(){
+  currSel =((currSel==0)
+                ?itemCount
+                :(currSel))-1 ;
+ //console.log((currSel*itemWidth));
+  $('.carousel ul')
+    .animate(
+      {marginLeft:
+       '-'
+       +(currSel*itemWidth)
+       +'px'}
+      ,speed);
+});
+});
