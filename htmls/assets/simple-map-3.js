5b73ef5e0398d6d0792b728bf6faa5a40cb382ea
Panpan <plin@tcd.ie>
2014-07-22 12:48:35 +0100

    figuring out map page with Ryan's map

---
+++ htmls/assets/simple-map-3.js
@@ -0,0 +1,18 @@
+
+		var map = L.map('map', {maxZoom: 18}).setView([51.495, -0.075], 14);
+		/* Instead of a tile layer, use a bitmap image */
+		var imageUrl = 'http://scss.tcd.ie/~plin/zombie/images/map-big.jpg';
+		var imageBounds = [[51.490, -0.130], [51.526, -0.030]];
+		L.imageOverlay(imageUrl, imageBounds).addTo(map);
+		L.marker([51.495, -0.075]).addTo(map)
+			.bindPopup("This is TZD map.");
+		var popup = L.popup();
+		function onMapClick(e) {
+			popup
+				.setLatLng(e.latlng)
+				.setContent("You clicked the map at " + e.latlng.toString())
+				.openOn(map);
+		}
+		map.on('click', onMapClick);
+		map.setMaxBounds(imageBounds);
+	
